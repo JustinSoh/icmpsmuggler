66b8f32a9de33eee956bb2b7581f64038faba938
JustinSoh <hoggersoh@gmail.com>
2024-02-06 14:49:18 +0800

    Add base64 encoding
    - to work on including more encoding
    - to work on receiver code
    - to drop a dropper eventually

--- packetCrafter.py
+++ packetCrafter.py
@@ -1,11 +1,18 @@
 import sys
+import base64
+
 from scapy.all import Ether, IP, ICMP, sr1, Raw
+
+def encodeData(input, type="b64"):
+    if type == "b64":
+        return base64.b64encode(input)
+    
 
 # set your source and destination port
 src = "10.211.55.2"
 dst = "10.211.55.5"
-payload = "thisisatestingpayload"
+payload = encodeData(b'thisisatestingpayload', "b64")
 
-p = sr1(IP(dst=dst , src=src)/ ICMP() / Raw(load=payload))
+p = sr1(IP(dst=dst , src=src)/ ICMP() / (payload))
 if p: 
     p.show()

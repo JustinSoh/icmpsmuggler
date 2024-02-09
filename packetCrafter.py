f05c7aa81336520f2bf4dd4fb864f0a91aca34b8
Justin <hoggersoh@gmail.com>
2024-02-09 14:14:32 +0800

    Add argparser

--- packetCrafter.py
+++ packetCrafter.py
@@ -1,6 +1,6 @@
 import sys
 import base64
-
+import argparse
 from scapy.all import Ether, IP, ICMP, sr1, Raw
 
 def encodeData(input, type="b64"):
@@ -8,11 +8,29 @@ def encodeData(input, type="b64"):
         return base64.b64encode(input)
     
 
-# set your source and destination port
-src = "10.211.55.2"
-dst = "10.211.55.5"
-payload = encodeData(b'thisisatestingpayload', "b64")
 
-p = sr1(IP(dst=dst , src=src)/ ICMP() / (payload))
-if p: 
-    p.show()
+def main(): 
+    
+    parser = argparse.ArgumentParser(
+        prog="ICMPSmuggler",
+        description="Smuggling data through ICMP"
+    )
+    
+    parser.add_argument('-s', '--src', required=True)
+    parser.add_argument('-d', '--dst', required=True)
+    parser.add_argument('-p', '--payload', required=True)
+    parser.add_argument('-e', '--enc')
+    
+    args = parser.parse_args()
+    src = args.src
+    dst = args.dst
+    payload = args.payload
+    if args.enc == "b64":
+        encodedPayload = encodeData(str.encode(payload), "b64")        
+        p = sr1(IP(dst=dst , src=src)/ ICMP() / (encodedPayload))
+        if p: 
+            p.show()
+
+
+if __name__ == "__main__":
+    main()

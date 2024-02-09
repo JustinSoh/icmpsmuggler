34d7f14936af2ffb7c99b6de5447f3b315f4dd04
Parallels <parallels@kali-linux-2022-2.localdomain>
2024-02-10 01:00:20 +0800

    Add print of payload

--- receiver.py
+++ receiver.py
@@ -6,12 +6,13 @@ from scapy.all import sniff
 
 def packet_handler(packet):
     if packet.haslayer('ICMP'):
-        print("Received packetttt:", packet.summary())
+        print("Received packetttt:", packet.payload)
         # Do something with the packet
 
 def main(): 
     # intialise parser and extract out info
-    sniff(filter="", prn=packet_handler, store=0)    
+    while True: 
+        sniff(filter="", prn=packet_handler, store=0)    
 
 if __name__ == "__main__":
     main()

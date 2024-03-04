906bed4a8f0e3426761b119d2964be8b1ec7ad6e
JustinSoh <parallels@kali-linux-2022-2.localdomain>
2024-03-04 13:38:44 +0800

    add receiver capabilities

--- receiver.py
+++ receiver.py
@@ -4,15 +4,35 @@ import binascii
 import argparse
 from scapy.all import sniff
 
-def packet_handler(packet):
-    if packet.haslayer('ICMP'):
-        print("Received packet:", packet.payload)
-        # Do something with the packet
+SESSION_START = False
+session_byte = b''
+def decodeData(input):
+    decoded = base64.b64decode(input)
+    return decoded
 
+def packet_handler(packet):
+    if packet.haslayer('ICMP') and packet['ICMP'].type == 8:
+        global SESSION_START
+        global session_byte
+
+        if b'IyNAQCEh' in packet.load:
+            SESSION_START = True
+            session_byte = b''
+
+
+        elif b'ISFAQCMj' in packet.load:
+            SESSION_START = False
+            session_byte = decodeData(session_byte)
+            print(session_byte)
+            
+        else: 
+            session_byte += packet.load
+
+            
 def main(): 
     # intialise parser and extract out info
     while True: 
-        sniff(filter="", prn=packet_handler, store=0)    
+        p = sniff(filter="icmp", prn=packet_handler, store=0)    
 
 if __name__ == "__main__":
     main()
